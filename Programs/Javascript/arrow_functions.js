0fdf7821f519bf651631708461ad305ff4ff3d2d
Kahlil Beasley <ksbeasle@asu.edu>
2019-06-18 21:19:41 -0700

    arrow functions

---
+++ Programs/Javascript/arrow_functions.js
@@ -0,0 +1,30 @@
+/*
+function sum(a, b){
+    return a + b
+}
+*/
+const sum = (a, b) => a + b
+console.log(sum(6,6))
+
+
+
+/*
+function positiveNumber(n){
+    if(n > 0) return true
+    else return false
+}
+*/
+const positiveNumber = number => number >= 0
+console.log(positiveNumber(-1))
+
+
+
+/*
+function randomNumber(){
+    return Math.random()
+}
+*/
+
+const randomNumber = () => Math.random()
+console.log(randomNumber())
+
