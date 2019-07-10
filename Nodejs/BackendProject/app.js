6fb6a4d56b4e720243194cbc074a64dd88b86437
Kahlil Beasley <ksbeasle@asu.edu>
2019-07-09 19:17:22 -0700

    connected to db

--- Nodejs/BackendProject/app.js
+++ Nodejs/BackendProject/app.js
@@ -1,30 +1,44 @@
-const Express = require("express");
+const express = require("express");
 const mongoose = require("mongoose");
 const BodyParser = require("body-parser");
+const app = express()
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb://localhost/AllGames", { useNewUrlParser: true })
+let db = mongoose.connection
+
+
+db.once('open', () => {
+    console.log("Connected to MongoDB")
+})
+
+db.on('error', (err) => {
+    console.log(err)
+})
+let schema = mongoose.Schema
+let gameSchema = new schema({
+    title:{type: String, required: true},
+    score:{type: Number, required: true},
+    rating:{type: String, required: true},
+    platform:{type: String, required: true},
+    genre:{type: String, required: true}
+}, {collection:'games'})
+
+var games = mongoose.model('games', gameSchema)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-app.get("/", async (req, res) => {
-    console.log("ok")
-});
+app.get('/', (req, res) => {
+    
+    
+})
 
 
 app.listen(8888, () => {
-    console.log("Listening on Port :8888...");
+    console.log("Listening on Port: 8888");
 });
 
 
