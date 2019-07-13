a0be81d7bc29034e546269ae26a7ac6abdcb0085
Kahlil Beasley <ksbeasle@asu.edu>
2019-07-12 18:45:37 -0700

    Phone number validation

--- Javascript/Algorithms/phoneValidation.js
+++ Javascript/Algorithms/phoneValidation.js
@@ -1,7 +1,7 @@
 
 var number = "888-888-8888"
 
-const phoneValidation = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+const phoneValidation = /\^?([0-9]{3})?[-]?([0-9]{3})[-]?([0-9]{4})$/;
 
 isValid = (phoneNumber) => {
     if(phoneNumber.match(phoneValidation)){
