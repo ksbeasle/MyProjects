0269917b9afcdd93643f396a4f4f23b0f1c11b9b
Kahlil Beasley <ksbeasle@asu.edu>
2019-08-12 17:16:48 -0700

    Algorithm practice

--- C#/stuff/stuff/Program.cs
+++ C#/stuff/stuff/Program.cs
@@ -6,7 +6,18 @@ namespace stuff
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String str = "HelloWorld";
+            Console.Write(reverseString(str));
+        }
+        public static String reverseString(String str)
+        { 
+
+            String revStr = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                revStr += str[i];
+            }
+            return revStr;
         }
     }
 }
---
+++ C#/stuff/stuff/reverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace stuff
+{
+    public class reverseString
+    {
+        public reverseString()
+        {
+            String str = "HelloWorld";
+
+            String revStr = "";
+            for(int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+
+        }
+    }
+}
