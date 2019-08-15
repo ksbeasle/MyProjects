6d5f352349cdfc42bdac753a3c3baf4b259b50ba
Kahlil Beasley <ksbeasle@asu.edu>
2019-08-15 13:36:25 -0700

    Algorithms

---
+++ C#/Algorithms/Algorithms/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static Algorithms.removeOddInt;
+namespace Algorithms
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            int[] seq = { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 };
+            removeOdd(seq);
+        }
+    }
+}
---
+++ C#/Algorithms/Algorithms/removeOddInt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Algorithms
+{
+    public class removeOddInt
+    {
+
+       public static int removeOdd(int[] seq)
+       {
+           int num = 0;
+           
+           for(int i = 0; i < seq.Length; i++)
+           {
+               for(int j = 0; j < seq.Length; j++)
+                {
+                    if(seq[i] == seq[j])
+                    {
+                        num++;
+                    }
+                }
+                if(num % 2 != 0)
+            {
+                    return seq[i];
+                }
+
+            }
+
+           
+           return num;
+       }
+   }
+}
